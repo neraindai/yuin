6da0feb347e690a79312593341cb4433a62e690e
Bomjan <shankargurung027@gmail.com>
2025-08-16 14:00:51 +0400

    changing header and section

--- src/components/ExtraComponent/BridalRelatedList.jsx
+++ src/components/ExtraComponent/BridalRelatedList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BridalRelatedCard from './BridalRelatedCard';
 import { bridal1, bridal2, bridal3, bridal4 } from '../../assets';
+import SectionHeader from './SectionHeader';
 
 const bridalArticle = [
     {
@@ -23,10 +24,10 @@ const bridalArticle = [
 const BridalRealtedList = () =>{
     return(
         <section className="container mx-automb-8">
-            <div className="flex flex-col mb-10">
-                <h2 className="text-center heading text-3xl font-bold mb-5 mt-0 font-yumincho">利用者の声</h2>
-                <p className="text-center relative mb-1 font-yumincho pl-15 pr-15 after:content-[''] after:flex  after:h-[1px] after:bg-[#D5E6DC] after:mt-[25px]">数多くのカップルがここで出会えました。実際にご利用いただいた皆さまの声から、あなたの理想にぴったりの出会いがきっと見つかります</p>
-            </div>
+            <SectionHeader 
+            sectionTitle="利用者の声"
+            shortDescription="数多くのカップルがここで出会えました。実際にご利用いただいた皆さまの声から、あなたの理想にぴったりの出会いがきっと見つかります"
+            />
             
                 <div className='grid grid-cols-[1fr] md:grid-cols-[repeat(4,_1fr)] gap-[15px] md:gap-[20px]'>
              {bridalArticle.map((article, index) => (
--- src/components/ExtraComponent/BrideGroomCardList.jsx
+++ src/components/ExtraComponent/BrideGroomCardList.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import BrideGroomCard from './BrideGroomCard';
 import { bridegroom1, bridegroom2 } from '../../assets';
+import SectionHeader from './SectionHeader';
 
 const BrideGroomCardList = () => {
   return (
     
     <section className="container mx-automb-8">
-      <div className="flex flex-col mb-10">
-        <h2 className="text-center heading text-3xl font-bold mb-5 mt-0 font-yumincho">利用者の声</h2>
-        <p className="text-center relative mb-1 font-yumincho pl-15 pr-15 after:content-[''] after:flex  after:h-[1px] after:bg-[#D5E6DC] after:mt-[25px]">数多くのカップルがここで出会えました。実際にご利用いただいた皆さまの声から、あなたの理想にぴったりの出会いがきっと見つかります</p>
-      </div>
+      <SectionHeader sectionTitle="利用者の声" 
+      shortDescription="数多くのカップルがここで出会えました。実際にご利用いただいた皆さまの声から、あなたの理想にぴったりの出会いがきっと見つかります"
+      />
 
-    <div className="grid grid-cols-2 gap-[10px]">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-[10px]">
       <BrideGroomCard
         title="新郎新婦"
         description="結婚式場のご予約やご相談に関するお問い合わせは、こちらより承っております"
